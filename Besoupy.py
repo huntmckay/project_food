1503697ea8e6e14e632cf507eefcd84060b97037
huntmckay <huntmckay@gmail.com>
2021-01-19 13:52:24 -0500

    should have commit a while ago

--- Besoupy.py
+++ Besoupy.py
@@ -11,12 +11,30 @@ soup = BeautifulSoup(URL.content, 'lxml')
 list_of_ingredients = soup.find('ul', class_='wprm-recipe-ingredients')
 
 
-ingred_amount = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-amount')
-ingred_unit = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-unit')
-ingred_name = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-name')
+# ingred_amount = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-amount')
+# ingred_unit = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-unit')
+# ingred_name = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-name')
 
 for child in list_of_ingredients.children:
-    # print(child.text.split(' '))
+    length_of_list = len(list_of_ingredients)
 
-    yeet = child.text.split(' ')
-    print(yeet[0])
+    x = 0
+    # print(child.text.split(' '))
+    item = child.text.split(' ')
+    # while x < length_of_list:
+    #     try:
+    #         amount = eval(item[0])
+    #         print(amount)
+    #         pass
+    #     except:
+    #         print(type(item[0]
+
+
+    # while x < ingredients:
+    # if ita[0] and ita[1] == int:
+    #     amount = eval(ita[0] + ita[1])
+    # else:
+    #     amount = eval(ita[0])
+    # print(amount)
+    # print(ita[0], ita[1:3])
+    print(child.text.split(' '))
