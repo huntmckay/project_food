b486481a1f2ccfea202bcc9dfc89bc14b788bcb3
huntmckay <huntmckay@gmail.com>
2021-01-20 09:40:24 -0500

    removed worthless code, still prints awkward list

--- Besoupy.py
+++ Besoupy.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import lxml
 
 URL = requests.get('https://www.thewholesomedish.com/the-best-classic-chili/')
 
@@ -14,27 +15,10 @@ list_of_ingredients = soup.find('ul', class_='wprm-recipe-ingredients')
 # ingred_amount = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-amount')
 # ingred_unit = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-unit')
 # ingred_name = list_of_ingredients.find('span', class_='wprm-recipe-ingredient-name')
-
 for child in list_of_ingredients.children:
     length_of_list = len(list_of_ingredients)
 
-    x = 0
-    # print(child.text.split(' '))
+    # print(length_of_list)
     item = child.text.split(' ')
-    # while x < length_of_list:
-    #     try:
-    #         amount = eval(item[0])
-    #         print(amount)
-    #         pass
-    #     except:
-    #         print(type(item[0]
 
-
-    # while x < ingredients:
-    # if ita[0] and ita[1] == int:
-    #     amount = eval(ita[0] + ita[1])
-    # else:
-    #     amount = eval(ita[0])
-    # print(amount)
-    # print(ita[0], ita[1:3])
     print(child.text.split(' '))
