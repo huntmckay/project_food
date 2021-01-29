bbe8629698f0a1e53b4353576fd5a59151f06f25
huntmckay <huntmckay@gmail.com>
2021-01-29 14:08:44 -0500

    adding new ansible stuff for later

--- Besoupy.py
+++ Besoupy.py
@@ -22,20 +22,24 @@ for child in list_of_ingredients.children:
     # then add index_0 and index_1
     # if type(eval(item[0])) == int or float : print('yeeyee')
     # elif type(eval(item[0])) != int or float : print('naynay')
-
+    myDict = {}
+    myDict['item'] = 'cow'
     try:
         # print('ITEM_0', '-', item[0], '   ', 'ITEM_1', '-', item[1]) #future debug
         item[0] = (eval(item[0]) + eval(item[1]))
         remove_index_1 = item.pop(1)
-        print(item)
+        # print(item)
+        myDict['item'].append(item)
         # print('removed', remove_index_1) # future debug
         
         # print('this is new item zero', '-', item[0]) #future debug
     except (NameError, SyntaxError):
         item[0] = eval(item[0])
-        print(item[0:])
+        # print(item[0:])
+        
+        myDict['item'].append(item)
     # print(child.text.split(' '))
-
+    print(myDict)
     #this is how i want the class output to be
     ingred_amount = item[0]
     ingred_unit = item[1]
