6f0d5dcae86045882465eba2ec462f9fd7647af4
huntmckay <huntmckay@gmail.com>
2021-01-29 14:09:05 -0500

    why didnt these commit

--- food.py
+++ food.py
@@ -2,22 +2,52 @@
 import os
 class Recipe:
 
-    num_of_ing = 0
-
     def __init__(self, amount, unit, name):
         
         #want to move float/eval logic to its own classmethod
-        self.amount = float(eval(amount))
+        self.amount = amount
         self.unit = unit
         self.name = name
 
-    @property #allows me to access this method as an attribute
-    def full_ingredient(self):
+    def scrapeurl(self, list_of_ingredients):
+        URL = requests.get('https://www.thewholesomedish.com/the-best-classic-chili/')
+        soup = BeautifulSoup(URL.content, 'lxml')
+        list_of_ingredients = soup.find('ul', class_='wprm-recipe-ingredients')
+        return list_of_ingredients
+
+    @property
+    def get_item(self):
+
+        for child in self.list_of_ingredients.children:
+
+            item = child.text.split(' ')
+            try:
+                # print('ITEM_0', '-', item[0], '   ', 'ITEM_1', '-', item[1]) #future debug
+                item[0] = (eval(item[0]) + eval(item[1]))
+                remove_index_1 = item.pop(1)
+                print(item)
+                # print('removed', remove_index_1) # future debug
+
+                # print('this is new item zero', '-', item[0]) #future debug
+            except (NameError, SyntaxError):
+                item[0] = eval(item[0])
+                print(item[0:])
+
+            self.amount = item[0]
+            self.unit = item[1]
+            self.name = item[2:]
+
         return '{} {} {}'.format(self.amount, self.unit, self.name)
+
     
     @classmethod
     def from_string(cls, ing_str):
         amount, unit, name = ing_str.split('-')
+        return cls(amount, unit, name)
+
+    @classmethod
+    def from_list(cls, ing_list):
+        amount, unit, name = ing_str.list()
         return cls(amount, unit, name)
 
     def __repr__(self):
@@ -39,15 +69,15 @@ class Recipe:
 ing_1 = Recipe('1', 'Cups', 'Cheddar')
 ing_2 = Recipe('1', 'Cups', 'Cheddar')
 
-# ing_str_1 = '1-cups-cheddar'
-# ing_str_2 = '2-tbsp-soy sauce'
-# ing_str_3 = '5.5-lbs-chicken'
-# ing_str_4 = '1/2-cups-milk'
+ing_str_1 = '1-cups-cheddar'
+ing_str_2 = '2-tbsp-soy sauce'
+ing_str_3 = '5.5-lbs-chicken'
+ing_str_4 = '1/2-cups-milk'
 
-ing_str_1 = 1, 'tablespoon', 'olive', 'oil'
-# [1, 'medium', 'yellow', 'onion', '-diced']
-# [1, 'pound', '90%', 'lean', 'ground', 'beef']
-# [2.5, 'tablespoons', 'chili', 'powder']
+# ing_str_1 = [1, 'tablespoon', 'olive', 'oil']
+# ing_str_2 = [1, 'medium', 'yellow', 'onion', '-diced']
+# ing_str_3 = [1, 'pound', '90%', 'lean', 'ground', 'beef']
+# ing_str_4 = [2.5, 'tablespoons', 'chili', 'powder']
 # [2, 'tablespoons', 'ground', 'cumin']
 # [2, 'tablespoons', 'granulated', 'sugar']
 # [2, 'tablespoons', 'tomato', 'paste']
