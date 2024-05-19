722686d6008f9911ac5bb0a2f276a238cdc71bcd
huntmckay <huntmckay@gmail.com>
2024-05-18 22:02:39 -0400

    large update, rip

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from recipe_scrapers import scrape_me
-from quantulum3 import parser
+import yaml
 
-s = scrape_me('https://www.allrecipes.com/recipe/158968/spinach-and-feta-turkey-burgers/')
+food_prices_file_path = 'food-prices.yaml'
+servings = input('how many servings do you want to make? default is 4')
+meal_plan_week = input('which meal plan do you want to use? (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)')
+recipe_dir_path = 'recipes'
 
-unfiltered = s.ingredients()
+# Open up food-prices.yaml
+with open(food_prices_file_path) as file:
+    food_prices = yaml.load(file, Loader=yaml.FullLoader)
 
-def filter(a):
-    filtered = {}
-    for i in a:
-        quant = parser.parse(i)
-        print(quant)
-print(filter(unfiltered))
+# Open up meal plan weeks work of yamls, append to one yaml in mem
+
+# Calculate total cost of meal plan
+
+
+# Create Shopping List
+
+# Write Shopping List to file
+
+# Print Shopping List
